970ca84b071607571b5c5ce65a62e917c5d77353
Michael Harris <mwharris7@gmail.com>
2020-07-02 11:41:30 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,4 @@
+
 require 'rake'
 require 'active_record'
 require 'yaml/store'
@@ -10,5 +11,9 @@ Bundler.require
 
 
 # put the code to connect to the database here
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 
 require_relative "../artist.rb"
